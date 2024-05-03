7ac3adea5c5afbf3640127622d0e5e17911e9bd5
Tianyu209 <146976390+Tianyu209@users.noreply.github.com>
2024-05-03 15:46:33 +0800

    Update api.py

--- gemini/api.py
+++ gemini/api.py
@@ -5,17 +5,30 @@ from IPython.display import Markdown
 import PIL.Image
 import os 
 from google.oauth2 import service_account
-
-GOOGLE_API_KEY=os.getenv('apikey')
+import cv2
+import base64
+from PIL import Image
+import numpy as np
+import io 
+GOOGLE_API_KEY=os.getenv('api')
 genai.configure(api_key=GOOGLE_API_KEY)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
 credentials = service_account.Credentials.from_service_account_file(
     'key.json', scopes=['https://www.googleapis.com/auth/cloud-platform']
 )
+model = genai.GenerativeModel('gemini-pro-vision')
+chat = model.start_chat(history=[])
+
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 app = Flask(__name__)
+
+def base642img(base64_string):
+    imgdata = base64.b64decode(str(base64_string))
+    img = Image.open(io.BytesIO(imgdata))
+    opencv_img= cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+    return opencv_img 
 
 @app.route('/api/respond', methods=['GET', 'POST'])
 def get_respond():
@@ -23,22 +36,24 @@ def get_respond():
         Prompt = request.get_json()['prompt']
     else:
         Prompt = request.args.get('prompt')
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(Prompt, stream=True)
+        
+    response = chat.send_message(Prompt, stream=True)
     response.resolve()
     return response.text
 
 @app.route('/api/recommend', methods=['GET', 'POST'])
 def get_recommend():
+    base64_string = request.get('base64')
+    img = base642img(base64_string)
     if request.method == 'POST':
         Prompt = request.get_json()['prompt']
     else:
         Prompt = request.args.get('prompt')
-    model = genai.GenerativeModel('gemini-pro-vision')
-    img = PIL.Image.open(r'image\menu.jpeg')
+    #img = PIL.Image.open(r'image\menu.jpeg')
     with open('prompt.txt', 'r') as f:
         text = f.read()
-    response = model.generate_content([text+Prompt,img])
+    chat.send_message(text+Prompt,stream=True)
+    response = chat.generate_content([text+Prompt,img])
     response.resolve()
     return response.text
 
